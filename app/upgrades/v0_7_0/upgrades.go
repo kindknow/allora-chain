b02f31e04abb28b99c8e708a248c323d4fa29e53
Guilherme Brandão <guilherme1237@gmail.com>
2024-12-02 10:30:54 +0000

    Added logs to the fee market config part of the migration

--- app/upgrades/v0_7_0/upgrades.go
+++ app/upgrades/v0_7_0/upgrades.go
@@ -37,24 +37,27 @@ func CreateUpgradeHandler(
 	keepers *keepers.AppKeepers,
 ) upgradetypes.UpgradeHandler {
 	return func(ctx context.Context, plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+		sdkCtx := sdk.UnwrapSDKContext(ctx)
 
 		vm, err := moduleManager.RunMigrations(ctx, configurator, vm)
 		if err != nil {
 			return vm, err
 		}
 
-		if err := ConfigureFeeMarketModule(ctx, keepers); err != nil {
+		sdkCtx.Logger().Info("CONFIGURING FEE MARKET MODULE")
+		if err := ConfigureFeeMarketModule(sdkCtx, keepers); err != nil {
 			return vm, err
 		}
+		sdkCtx.Logger().Info("FEE MARKET MODULE CONFIGURED")
 
 		return vm, nil
 	}
 }
 
-func ConfigureFeeMarketModule(ctx context.Context, keepers *keepers.AppKeepers) error {
-	sdkCtx := sdk.UnwrapSDKContext(ctx)
+func ConfigureFeeMarketModule(ctx sdk.Context, keepers *keepers.AppKeepers) error {
+	ctx.Logger().Info("SETTING PARAMETERS FOR FEE MARKET MODULE")
 
-	feeMarketParams, err := keepers.FeeMarketKeeper.GetParams(sdkCtx)
+	feeMarketParams, err := keepers.FeeMarketKeeper.GetParams(ctx)
 	if err != nil {
 		return err
 	}
@@ -63,16 +66,16 @@ func ConfigureFeeMarketModule(ctx context.Context, keepers *keepers.AppKeepers) 
 	feeMarketParams.FeeDenom = params.BaseCoinUnit
 	feeMarketParams.DistributeFees = true
 	feeMarketParams.MinBaseGasPrice = cosmosmath.LegacyMustNewDecFromStr("10")
-	if err := keepers.FeeMarketKeeper.SetParams(sdkCtx, feeMarketParams); err != nil {
+	if err := keepers.FeeMarketKeeper.SetParams(ctx, feeMarketParams); err != nil {
 		return err
 	}
-
-	state, err := keepers.FeeMarketKeeper.GetState(sdkCtx)
+	ctx.Logger().Info("SETTING STATE FOR FEE MARKET MODULE")
+	state, err := keepers.FeeMarketKeeper.GetState(ctx)
 	if err != nil {
 		return err
 	}
 
 	state.BaseGasPrice = cosmosmath.LegacyMustNewDecFromStr("10")
 
-	return keepers.FeeMarketKeeper.SetState(sdkCtx, state)
+	return keepers.FeeMarketKeeper.SetState(ctx, state)
 }
